005c793e6192e9b9bf0f756024e46f9fe736abae
mhenman <mark.henman@gmail.com>
2020-01-27 16:13:04 -0600

    Added color matching code

--- Adafruit_rgb_sensor_testing_2020.ino
+++ Adafruit_rgb_sensor_testing_2020.ino
@@ -15,37 +15,64 @@
  ***************************************************************************/
 
 //the pin that the interrupt is attached to
-#define INT_PIN 3
+#define INT_PIN 13
 
 
 #include <Adafruit_APDS9960.h>
 
 // Add neopixel support so we can illuminate the target.
 #include <Adafruit_NeoPixel.h>
+#include "ColorSample.h"
 
 #define NEO_PIN        6 // On Trinket or Gemma, suggest changing this to 1
-#define NUMPIXELS     12 // Popular NeoPixel ring size
+#define NUMPIXELS     16 // Popular NeoPixel ring size
 #define ONBOARD_NEO_PIN  40
 
 #define PIN_OUT1          8
 #define PIN_OUT2          9
 #define PIN_VALID        10
 
+#define RED_CALIBRATE_PIN 2
+#define GREEN_CALIBRATE_PIN 3
+#define BLUE_CALIBRATE_PIN 4
+#define YELLOW_CALIBRATE_PIN 5
+
 Adafruit_APDS9960 apds;
 Adafruit_NeoPixel pixels(NUMPIXELS, NEO_PIN, NEO_GRB + NEO_KHZ800);
 Adafruit_NeoPixel boardPixel(1, ONBOARD_NEO_PIN, NEO_GRB + NEO_KHZ800);
 
+long count = 0;
+long timerStart = 0;
+long readsAverage = 0;
+
+ColorSample targetColors[] = {
+  {"Red", 63, 12, 24, 0, 0, 100, 0, 0},
+  {"Green", 15, 44, 42, 0, 1, 0, 100, 0},
+  {"Blue", 8, 32, 58, 1, 0, 0, 0, 100},
+  {"Yellow", 36, 38, 25, 1, 1, 50, 50, 0}
+  };
+  
 void setup() {
+//  while (!Serial); // Wait for Serial port to initialize.
   Serial.begin(115200);
   pinMode(INT_PIN, INPUT_PULLUP);
   pinMode(PIN_OUT1, OUTPUT);
   pinMode(PIN_OUT2, OUTPUT);
   pinMode(PIN_VALID, OUTPUT);
 
+  pinMode(RED_CALIBRATE_PIN, INPUT_PULLUP);
+  pinMode(GREEN_CALIBRATE_PIN, INPUT_PULLUP);
+  pinMode(BLUE_CALIBRATE_PIN, INPUT_PULLUP);
+  pinMode(YELLOW_CALIBRATE_PIN, INPUT_PULLUP);
+
+  Wire.setClock(1000000);
+
+  Serial.println("Starting...");
   if(!apds.begin()){
     Serial.println("failed to initialize device! Please check your wiring.");
+  } else {
+    Serial.println("Device initialized!");
   }
-  else Serial.println("Device initialized!");
 
   //enable color sensign mode
   apds.enableColor(true);
@@ -54,7 +81,8 @@ void setup() {
   apds.enableProximity(true);
 
   //set the interrupt threshold to fire when proximity reading goes above 10
-  apds.setProximityInterruptThreshold(0, 10);
+  apds.setProximityInterruptThreshold(0, 5);
+  apds.setADCIntegrationTime(2);
 
   //enable the proximity interrupt
   apds.enableProximityInterrupt();
@@ -67,21 +95,47 @@ void setup() {
 
     // Here we're using a moderately color that compensates for the slightly 
     // blueish tint if we set them to all the same color:
-    pixels.setPixelColor(i, pixels.Color(60, 80, 50));
+    pixels.setPixelColor(i, 70, 60, 50);
 
   }
   pixels.show();   // Send the updated pixel colors to the hardware.
 
+  Serial.println("NeoPixel Ring Started");
   boardPixel.begin();
-  boardPixel.setBrightness(127);
+  boardPixel.setBrightness(50);
   boardPixel.clear();
 
+  Serial.println("Setup complete.");
 }
 
 void outputValues(int bit1, int bit2, int valid) {
   digitalWrite(PIN_OUT1, bit1); // Output bit 1
   digitalWrite(PIN_OUT2, bit2); // Output bit 2
   digitalWrite(PIN_VALID, valid);
+}
+
+void checkForCalibration(int r, int g, int b) {
+  if (!digitalRead(RED_CALIBRATE_PIN)) {
+    Serial.println("Calibrating Red.");
+    targetColors[0].red = r;
+    targetColors[0].green = g;
+    targetColors[0].blue = b;
+  } else if (!digitalRead(GREEN_CALIBRATE_PIN)) {
+    Serial.println("Calibrating Green.");
+    targetColors[1].red = r;
+    targetColors[1].green = g;
+    targetColors[1].blue = b;
+  } else if (!digitalRead(BLUE_CALIBRATE_PIN)) {
+    Serial.println("Calibrating Blue.");
+    targetColors[2].red = r;
+    targetColors[2].green = g;
+    targetColors[2].blue = b;
+  } else if (!digitalRead(YELLOW_CALIBRATE_PIN)) {
+    Serial.println("Calibrating Yellow.");
+    targetColors[3].red = r;
+    targetColors[3].green = g;
+    targetColors[3].blue = b;
+  }
 }
 
 void loop() {
@@ -95,21 +149,26 @@ void loop() {
   } else {
     // If we're not close to anything, then just pause for a little bit 
     // and try again from the start.
-    delay(5);
-    //outputValues(0, 0);
-    //boardPixel.setPixelColor(0, pixels.Color(0, 0, 0));
-    //boardPixel.show();
+    delay(1);
     return;
   }
   
   //wait for color data to be ready
   while(!apds.colorDataReady()){
-    delay(1);
+    //delay(1);
   }
 
+  long currentTime = millis();
+  if (currentTime - timerStart > 1000) {
+    readsAverage = count;
+    count = 0;
+    timerStart = currentTime;
+  }
+  
   //get the data and print the different channels
   apds.getColorData(&r, &g, &b, &c);
-
+  count++;
+  
   // Normalize the values:
   int normRed   = (100 * r) / (r + g + b);
   int normGreen = (100 * g) / (r + g + b);
@@ -124,38 +183,21 @@ void loop() {
   
   Serial.print(" blue: ");
   Serial.print(normBlue);
-  
+
+  Serial.print(" sample rate: ");
+  Serial.print(readsAverage);
+
+  int distance = 0;
+  ColorSample *target = findClosestColor(targetColors, normRed, normGreen, normBlue, distance);
+  //if (distance < 15) {
+    Serial.print("\t");Serial.print(target->name);Serial.print("  -->  "); Serial.print(distance);
+    outputValues(target->out1, target->out2, 1);
+    boardPixel.setPixelColor(0, pixels.Color(target->dispRed, target->dispGreen, target->dispBlue));
+    boardPixel.show();
+  //}
   Serial.println();
 
-  // This is using raw values, but we should switch to normalized values:
-  if (normGreen - normRed > 20 && normGreen - normBlue > 5) {
-    Serial.println("GREEN");
-    outputValues(0, 1, 1);
-    boardPixel.setPixelColor(0, pixels.Color(0, 100, 0));
-    boardPixel.show();
-  } else if (normRed - normGreen > 20 && abs(normGreen - normBlue) < 10) {
-    Serial.println("RED");
-    outputValues(0, 0, 1);
-    boardPixel.setPixelColor(0, pixels.Color(100, 0, 0));
-    boardPixel.show();
-  } else if (abs(normRed - normGreen) < 5 && normGreen - normBlue > 10) {
-    Serial.println("YELLOW");
-    outputValues(1, 1, 1);
-    boardPixel.setPixelColor(0, pixels.Color(50, 50, 0));
-    boardPixel.show();
-  } else if (normGreen - normRed > 10 && (normBlue - normGreen) > 10) {
-    Serial.println("BLUE");
-    outputValues(1, 0, 1);
-    boardPixel.setPixelColor(0, pixels.Color(0, 0, 100));
-    boardPixel.show();
-  } else {
-    outputValues(0, 0, 0);
-    boardPixel.setPixelColor(0, pixels.Color(0, 0, 0));
-    boardPixel.show();
-  }
-
-  //pixels.show();
-
+  checkForCalibration(normRed, normGreen, normBlue);
   
-  delay(5);
+  delay(1);
 }
